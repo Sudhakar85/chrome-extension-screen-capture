c347aa728884dee8eccb6c3e1177738ea9797357
Sudhakar Annamalai <a.sudhakar85@gmail.com>
2017-10-14 16:21:07 -0500

    Update AddHostToRegistry.bat

--- NativeChromHost/NativeChromHost/AddHostToRegistry.bat
+++ NativeChromHost/NativeChromHost/AddHostToRegistry.bat
@@ -1,2 +1,2 @@
-﻿Reg Add "HKEY_CURRENT_USER\SOFTWARE\Google\Chrome\NativeMessagingHosts\dell_chrome_screen_capture_save" /ve /t REG_SZ /d "C:\Sudhakar\POC\NativeChromHost\NativeChromHost\dell_chrome_screen_capture_save.json" /f
-Reg Add "HKEY_LOCAL_MACHINE\SOFTWARE\Google\Chrome\NativeMessagingHosts\dell_chrome_screen_capture_save" /ve /t REG_SZ /d "C:\Sudhakar\POC\NativeChromHost\NativeChromHost\dell_chrome_screen_capture_save.json" /f
+﻿Reg Add "HKEY_CURRENT_USER\SOFTWARE\Google\Chrome\NativeMessagingHosts\chrome_screen_capture_save" /ve /t REG_SZ /d "C:\Sudhakar\POC\NativeChromHost\NativeChromHost\chrome_screen_capture_save.json" /f
+Reg Add "HKEY_LOCAL_MACHINE\SOFTWARE\Google\Chrome\NativeMessagingHosts\chrome_screen_capture_save" /ve /t REG_SZ /d "C:\Sudhakar\POC\NativeChromHost\NativeChromHost\chrome_screen_capture_save.json" /f
